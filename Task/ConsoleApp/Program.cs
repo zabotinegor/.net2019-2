73589ed72e589d482178230e306c8051e3359cef
Егор Заботин <zabotinegor@mail.ru>
2019-12-06 16:11:42 +0300

    added resources

--- Task/ConsoleApp/Program.cs
+++ Task/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Resources.Resources.HelloWord);
 
             Console.ReadKey();
         }
