ad68b47fb1c67d625b9e53ee2e257fb87f1caac1
Yahor Zabotsin <Yahor_Zabotsin@epam.com>
2019-12-09 21:04:49 +0300

    start menu

--- Task/UserInterface/Task.ConsoleApp/Program.cs
+++ Task/UserInterface/Task.ConsoleApp/Program.cs
@@ -6,7 +6,25 @@ namespace Task.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Task.CommonTypes.Resources.Resources.HelloMessage);
             Console.ReadKey();
+
+            bool showMenu = true;
+
+            while(showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool MainMenu()
+        {
+            bool result = true;
+            int state = 0;
+
+            return result;
         }
     }
 }
