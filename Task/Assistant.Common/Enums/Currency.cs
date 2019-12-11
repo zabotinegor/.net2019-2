065bd3bcd777161467ae48a59bd447a37bc4089a
Егор Заботин <zabotinegor@mail.ru>
2019-12-11 11:04:30 +0300

    Created Models

---
+++ Task/Assistant.Common/Enums/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace Assistant.Common.Enums
+{
+    public enum Currency
+    {
+        USD,
+        BUB,
+        BYN
+    }
+}
---
+++ Task/Assistant.Common/Enums/Direction.cs
@@ -0,0 +1,8 @@
+﻿namespace Assistant.Common.Enums
+{
+    public enum Direction
+    {
+        IN,
+        OUT
+    }
+}
